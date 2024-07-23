9798da7d0eb0e3e9aa4d6d874cb033870e080b1a
Alex <udinkik@mail.ru>
2024-07-23 20:33:26 +0300

    add Solution Javasyntax task 0201

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0201/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task02.task0201;
+
+/* 
+«Я по объявлению…»
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        int intValue = 4;
+        int numericValue = 5;
+        int size = 6;
+        int number = 7;
+    }
+}

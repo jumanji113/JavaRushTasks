ad779c788d3b4c83f27cd54a2f5b0a950f14866c
Alex <udinkik@mail.ru>
2024-07-25 19:56:54 +0300

    add Solution Javasyntax task 0401

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0401/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task04.task0401;
+
+/* 
+Я никогда не буду работать за копейки
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int num = 100;
+            String quote = "Я никогда не буду работать за копейки. Амиго";
+        while(num > 0) {
+            System.out.println(quote);
+            num = num - 1;
+        }
+
+        //напишите тут ваш код
+
+    }
+}

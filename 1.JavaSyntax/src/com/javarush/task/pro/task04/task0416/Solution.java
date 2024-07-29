892c4055b421f0c1bdb9e29e9fa3261c15cf1930
Alex <udinkik@mail.ru>
2024-07-29 08:45:15 +0300

    add Solution Javasyntax task 0416

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0416/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task04.task0416;
+
+import java.util.Scanner;
+
+/* 
+Share a Coke
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Scanner console = new Scanner(System.in);
+        int a = console.nextInt();
+        int b = console.nextInt();
+        System.out.println(a * 1.0 / b);
+    }
+}

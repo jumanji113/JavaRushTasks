8b9f69bf286eb7fc0c8e4cf8633be3d5bfe2823e
Alex <udinkik@mail.ru>
2024-08-06 12:12:49 +0300

    add Solution Javasyntax task 0602

--- 1.JavaSyntax/src/com/javarush/task/pro/task06/task0602/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0602/Solution.java
@@ -7,6 +7,10 @@ package com.javarush.task.pro.task06.task0602;
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
+        printBot();
+    }
+
+    public static void printBot() {
         System.out.println("Я — поэт, зовусь я Цветик.");
         System.out.println("От меня вам всем приветик.");
     }

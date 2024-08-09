f9e666db1e0c939fb8aafc7ea8e06f92d06de050
Alex <udinkik@mail.ru>
2024-08-09 18:31:11 +0300

    add Solution Javasyntax task 0702

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0702/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task07.task0702;
+
+/* 
+Максимальное целое
+*/
+
+public class Solution {
+    //напишите тут ваш код
+    byte n1 = 127;
+    short n2 = 32767;
+    int n3 = 2147483647;
+    long n4 = 9223372036854775807L;
+}

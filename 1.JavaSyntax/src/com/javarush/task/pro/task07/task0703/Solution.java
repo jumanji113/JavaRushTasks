c8f5a05d57175573d44032f8d67d601c5d7034be
Alex <udinkik@mail.ru>
2024-08-09 18:36:23 +0300

    add Solution Javasyntax task 0703

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0703/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task07.task0703;
+
+/* 
+Минимальное целое
+*/
+
+public class Solution {
+    //напишите тут ваш код
+    byte n1 = -128;
+    short n2 = -32768;
+    int n3 = -2147483648;
+    long n4 = -9223372036854775808L;
+}

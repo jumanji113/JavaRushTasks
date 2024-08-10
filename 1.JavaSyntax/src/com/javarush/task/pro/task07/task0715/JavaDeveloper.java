75a49c697aa042de953edf056c69a450fe0a1e01
Alex <udinkik@mail.ru>
2024-08-10 19:53:44 +0300

    add Solution Javasyntax task 0715

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0715/JavaDeveloper.java
@@ -0,0 +1,10 @@
+package com.javarush.task.pro.task07.task0715;
+
+import com.javarush.task.pro.task07.task0713.Human;
+
+public class JavaDeveloper extends Human {
+
+    public void code(){
+        System.out.println("Я умею общаться на Java.");
+    }
+}

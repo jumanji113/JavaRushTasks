93d6070bcb04b92ed1fb57d037fcc8fead76fe54
Alex <udinkik@mail.ru>
2024-09-02 13:22:27 +0300

    add Solution Javasyntax task 0809

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0809/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task08.task0809;
+
+public class Solution {
+
+    public static void main(String[] args) throws InterruptedException {
+        printNumbers();
+    }
+
+    public static void printNumbers() throws InterruptedException {
+        System.out.println(1);
+        for (int i = 2; i <= 10; i++) {
+            Thread.sleep(1, 300000);
+            System.out.println(i);
+        }
+    }
+}

e0521479e300414377b37901b27129249b0ebdc2
Alex <udinkik@mail.ru>
2024-10-06 22:59:08 +0300

    add Solution Javasyntax task 1010

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1020/Computer.java
@@ -0,0 +1,8 @@
+package com.javarush.task.pro.task10.task1020;
+
+public class Computer {
+    Keyboard kyeboard = new Keyboard();
+    Monitor monitor = new Monitor();
+    Mouse kye = new Mouse();
+    SystemUnit sys = new SystemUnit();
+}

36943026b37e2d064443b258d728c106b8b9395d
Alex <udinkik@mail.ru>
2025-03-31 18:57:32 +0300

    add Solution Javasyntax task 1021

--- 1.JavaSyntax/src/com/javarush/task/pro/task10/task1021/Car.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1021/Car.java
@@ -10,5 +10,10 @@ public class Car {
     private String color;
 
     //напишите тут ваш код
+    public void initialize(String model, int year, String color){
+        this.model = model;
+        this.year = year;
+        this.color = color;
+    }
 
 }

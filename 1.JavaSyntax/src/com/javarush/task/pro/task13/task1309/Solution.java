6110c31e2e71fa0f1747a6af6f8c1eee5508a213
Alex <udinkik@mail.ru>
2025-04-11 17:36:34 +0300

    add Solution Javasyntax task 1309

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1309/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.pro.task13.task1309;
+
+import java.util.HashMap;
+
+/* 
+Успеваемость студентов
+*/
+
+public class Solution {
+    public static HashMap<String, Double> grades = new HashMap<>();
+
+    public static void main(String[] args) {
+        addStudents();
+        System.out.println(grades);
+    }
+
+    public static void addStudents() {
+        //напишите тут ваш код
+        grades.put("Levan", 3.3);
+        grades.put("Ignat", 3.3);
+        grades.put("Okko", 3.3);
+        grades.put("Ivi", 3.3);
+        grades.put("KINO", 3.3);
+    }
+}

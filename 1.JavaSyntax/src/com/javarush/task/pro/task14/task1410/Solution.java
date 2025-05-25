a7a87b324f3a6e40216e009ea433fdc24865e049
Alex <udinkik@mail.ru>
2025-05-25 16:12:44 +0300

    add Solution Javasyntax task 1410

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task14/task1410/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.pro.task14.task1410;
+
+import java.io.FileNotFoundException;
+import java.nio.file.FileSystemException;
+
+/* 
+Оборачивание исключений
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        copyFile("book.txt", "book_final_copy.txt");
+        copyFile("book_final_copy.txt", "book_last_copy.txt");
+    }
+
+    static void copyFile(String sourceFile, String destinationFile) {
+        try {
+            FileUtils.readFile(sourceFile);
+        } catch (RuntimeException | FileNotFoundException e){
+            throw new RuntimeException(e);
+        }
+        try{
+            FileUtils.writeFile(destinationFile);
+        } catch (RuntimeException  | FileSystemException exception){
+            throw new RuntimeException(exception);
+        }
+
+        //напишите тут ваш код
+    }
+}
